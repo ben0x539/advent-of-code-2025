e24b43556f9013a15ed203e0785a429cf7fa1956
Benjamin Herr <git@1d6.org>
2025-12-08 02:43:28 -0800

    day 8 apparently it's called a disjoint-set forest when you do it right

--- src/bin/day-08/main.rs
+++ src/bin/day-08/main.rs
@@ -9,6 +9,7 @@ use anyhow::bail;
 
 type Xyz = (i64, i64, i64);
 
+#[derive(Debug)]
 struct JunctionBox {
     pos: Xyz,
     circuit: usize,
@@ -24,6 +25,26 @@ fn main() -> Result<()> {
 fn get_sqr_dist(a: Xyz, b: Xyz) -> i64 {
     let (dx, dy, dz) = (a.0 - b.0, a.1 - b.1, a.2 - b.2);
     dx*dx + dy*dy + dz*dz
+}
+
+fn resolve_circuit(boxes: &mut Vec<JunctionBox>, i: usize) -> usize {
+    let mut p = boxes[i].circuit;
+    loop {
+        if p == boxes[p].circuit {
+            break;
+        }
+
+        p = boxes[p].circuit;
+    }
+
+    let mut i = i;
+    while i != p {
+        let q = boxes[i].circuit;
+        boxes[i].circuit = p;
+        i = q;
+    }
+
+    return p;
 }
 
 fn go(p: &Path, connect_all: bool) -> Result<i64> {
@@ -59,37 +80,47 @@ fn go(p: &Path, connect_all: bool) -> Result<i64> {
 
     let mut merges = 0;
 
+    //for v in boxes.iter().enumerate() {
+    //    eprintln!("{v:?}");
+    //}
+
     for (connections_count, &(_dist, i, j)) in dists.iter().enumerate() {
         if !connect_all && connections_count >= connections_limit {
             break;
         }
 
-        let circuit_i = boxes[i].circuit;
-        let circuit_j = boxes[j].circuit;
+        let circuit_i = resolve_circuit(&mut boxes, i);
+        let circuit_j = resolve_circuit(&mut boxes, j);
 
-        if circuit_i == circuit_j { continue; }
+        if circuit_i == circuit_j {
+            //eprintln!("boxes {i}, {j} are already both in circuit {circuit_i}");
+            continue;
+         }
 
-        for b in &mut boxes {
-            if b.circuit == circuit_j {
-                b.circuit = circuit_i;
-            }
-        }
+        //eprintln!("connecting boxes {i}, {j} in circuit {circuit_i}");
+        boxes[circuit_j].circuit = circuit_i;
+        //eprintln!("{:?}", boxes.iter().map(|b| b.circuit).collect::<Vec<_>>());
 
         if connect_all {
             merges += 1;
             if boxes.len() - merges <= 1 {
-                eprintln!("last boxes are {:?}; {:?}", boxes[i].pos, boxes[j].pos);
+                //eprintln!("last boxes are {:?}; {:?}", boxes[i].pos, boxes[j].pos);
                 return Ok(boxes[i].pos.0 * boxes[j].pos.0);
             }
         }
     }
 
     let mut circuits = BTreeMap::new();
-    for b in &boxes {
-        *circuits.entry(b.circuit).or_default() += 1;
+    for i in 0..boxes.len() {
+        *circuits.entry(resolve_circuit(&mut boxes, i)).or_default() += 1;
     }
     let mut circuit_sizes: Vec<_> = circuits.iter().map(|(_, &v)| v).collect();
+    //dbg!(&circuit_sizes);
     circuit_sizes.sort();
+
+    //for v in boxes.iter().enumerate() {
+    //    eprintln!("{v:?}");
+    //}
 
     Ok(circuit_sizes.iter().rev().take(3).product())
 }
